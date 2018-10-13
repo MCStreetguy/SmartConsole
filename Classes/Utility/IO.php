02e32ede0305445c8306465dcfc2894f190d140a
Maximilian Schmidt <m.schmidt@obis-concept.de>
2018-10-13 15:27:57 +0200

    Improved the padding method

--- Classes/Utility/IO.php
+++ Classes/Utility/IO.php
@@ -150,15 +150,15 @@ class IO extends RawIO
      * Prompt the user for input.
      *
      * @param string $question The message to display to the user before prompting
-     * @param bool $forceAnswer Force the user to input something, if the result is empty prompt again
-     * @param string $default The default value of the prompt if the user omits entering anything
-     * @param bool $hint Give the user a hint about the default value
-     * @param bool $multiline Make the input span over multiple lines and wait for ^D before returning
-     * @param bool $hidden Make the user input hidden (i.e. for a password prompt)
-     * @param string $color The foreground color to use
-     * @param string $background The background color to use
-     * @param string $hintColor The foreground color to use for the hint, if shown
-     * @param string $hintBackground The background color to use for the hint, if shown
+     * @param bool|null $forceAnswer Force the user to input something, if the result is empty prompt again
+     * @param string|null $default The default value of the prompt if the user omits entering anything
+     * @param bool|null $hint Give the user a hint about the default value
+     * @param bool|null $multiline Make the input span over multiple lines and wait for ^D before returning
+     * @param bool|null $hidden Make the user input hidden (i.e. for a password prompt)
+     * @param string|null $color The foreground color to use
+     * @param string|null $background The background color to use
+     * @param string|null $hintColor The foreground color to use for the hint, if shown
+     * @param string|null $hintBackground The background color to use for the hint, if shown
      * @return string
      * @throws \InvalidArgumentException
      */
@@ -211,11 +211,11 @@ class IO extends RawIO
      *
      * @param string $question The message to display to the user before prompting
      * @param array $answers The possible answer the user may give
-     * @param string $default The default value of the prompt if the user omits entering anything
-     * @param bool $hint Give the user a hint of all possibilities
-     * @param bool $strict Make the comparison case-sensitive
-     * @param string $color The foreground color to use
-     * @param string $background The background color to use
+     * @param string|null $default The default value of the prompt if the user omits entering anything
+     * @param bool|null $hint Give the user a hint of all possibilities
+     * @param bool|null $strict Make the comparison case-sensitive
+     * @param string|null $color The foreground color to use
+     * @param string|null $background The background color to use
      * @return string
      * @throws \InvalidArgumentException
      */
@@ -246,8 +246,8 @@ class IO extends RawIO
      * Ask the user for confirmation.
      *
      * @param string $question The message to display to the user before prompting
-     * @param string $color The foreground color to use
-     * @param string $background The background color to use
+     * @param string|null $color The foreground color to use
+     * @param string|null $background The background color to use
      * @return bool
      * @throws \InvalidArgumentException
      */
@@ -293,7 +293,7 @@ class IO extends RawIO
      * Print a simple table to the terminal.
      *
      * @param array $data An associative array of data mappings
-     * @param bool $borderless Make the table borderless
+     * @param bool|null $borderless Make the table borderless
      * @return void
      */
     public function simpleTable(array $data, bool $borderless = false)
@@ -315,7 +315,7 @@ class IO extends RawIO
      * Print a complex table to the terminal.
      *
      * @param array $data An array of associative arrays of data mappings (the keys from the first sub-array are used as header row)
-     * @param bool $borderless Make the table borderless
+     * @param bool|null $borderless Make the table borderless
      * @return void
      * @throws \InvalidArgumentException
      */
@@ -342,9 +342,9 @@ class IO extends RawIO
      * Print data in columns to the terminal (e.g. like 'ls' does).
      *
      * @param array $data An array of data to print
-     * @param int $count The amount of columns to be printed (this is recognized automatically if unset)
-     * @param string $color The foreground color to use
-     * @param string $background The background color to use
+     * @param int|null $count The amount of columns to be printed (this is recognized automatically if unset)
+     * @param string|null $color The foreground color to use
+     * @param string|null $background The background color to use
      * @return void
      * @throws \InvalidArgumentException
      */
@@ -363,24 +363,38 @@ class IO extends RawIO
      * Print padded data to the terminal.
      *
      * @param array $data An associative array of data mappings
-     * @param int $size The space between the keys and values (this is recognized automatically if unset)
-     * @param string $character The character symbol to use as spacing
-     * @param string $color The foreground color to use
-     * @param string $background The background color to use
-     * @param string $resultColor The foreground color to use for the result column
-     * @param string $resultBackground The background color to use for the result column
+     * @param int|string|null $size The space between the keys and values (this is recognized automatically if unset)
+     * @param string|null $character The character symbol to use as spacing
+     * @param string|null $color The foreground color to use
+     * @param string|null $background The background color to use
+     * @param string|null $resultColor The foreground color to use for the result column
+     * @param string|null $resultBackground The background color to use for the result column
      * @return void
      * @throws \InvalidArgumentException
      */
     public function padding(
         array $data,
-        int $size = null,
+        $size = '+5',
         string $character = null,
         string $color = null,
         string $background = null,
         string $resultColor = null,
         string $resultBackground = null
     ) {
+        if (is_string($size) && substr($size, 0, 1) === '+') {
+            $add = intval(substr($size, 1));
+            $size = null;
+        } elseif ($size === null || is_int($size)) {
+            $add = 5;
+        } else {
+            $type = gettype($size);
+
+            throw new \InvalidArgumentException(
+                "Invalid 'size' argument, expected 'string' or 'int', recieved '$type'!",
+                1539436806
+            );
+        }
+
         if ($size === null) {
             $longestKey = 0;
 
@@ -390,7 +404,7 @@ class IO extends RawIO
                 }
             }
 
-            $size = $longestKey + 5;
+            $size = $longestKey + $add;
         }
 
         if (!empty($character)) {
@@ -426,10 +440,10 @@ class IO extends RawIO
     /**
      * Print a border-line to the terminal.
      *
-     * @param string $pattern The pattern to print
-     * @param int $size The desired width of the line (defaults to the width of the terminal)
-     * @param string $color The foreground color to use
-     * @param string $background The background color to use
+     * @param string|null $pattern The pattern to print
+     * @param int|null $size The desired width of the line (defaults to the width of the terminal)
+     * @param string|null $color The foreground color to use
+     * @param string|null $background The background color to use
      * @return void
      * @throws \InvalidArgumentException
      */
@@ -443,9 +457,9 @@ class IO extends RawIO
     /**
      * Start a progress bar in the terminal screen.
      *
-     * @param int $total The maximum steps of the progress bar
-     * @param string $color The foreground color to use
-     * @param string $background The background color to use
+     * @param int|null $total The maximum steps of the progress bar
+     * @param string|null $color The foreground color to use
+     * @param string|null $background The background color to use
      * @return void
      * @throws \InvalidArgumentException
      */
@@ -463,7 +477,7 @@ class IO extends RawIO
      * NOTE: This requires to start a progress bar first!
      *
      * @param int $progress The current progress to set
-     * @param string $label An optional label, describing the current step
+     * @param string|null $label An optional label, describing the current step
      * @return void
      * @throws \InvalidArgumentException
      */
@@ -480,7 +494,7 @@ class IO extends RawIO
      * NOTE: This requires to start a progress bar first!
      *
      * @param int $step The number of steps to advance the progress by
-     * @param string $label An optional label, describing the current step
+     * @param string|null $label An optional label, describing the current step
      * @return void
      * @throws \InvalidArgumentException
      */
@@ -496,7 +510,7 @@ class IO extends RawIO
      *
      * NOTE: This requires to start a progress bar first!
      *
-     * @param string An optional label, describing the finalization
+     * @param string|null An optional label, describing the finalization
      * @return void
      * @throws \InvalidArgumentException
      */
